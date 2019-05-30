f25a234a09a0dd234fd211b2e0916a2b2fdb2515
Pedro Castro <pedrohlcastro@gmail.com>
2019-05-30 15:30:22 -0300

    Add Debug Mode On SQM Querys

--- errors.go
+++ errors.go
@@ -2,6 +2,6 @@ package sqm
 
 import "fmt"
 
-var ErrorInvalidType = fmt.Errorf("Invalid Type")
+var ErrorInvalidType = fmt.Errorf("Invalid Type - Params have an unsupported type")
 var ErrorMultipleResults = fmt.Errorf("Passed a pointer for a struct but returned multiple results, check query or use a slice")
-var ErrorNoRows = fmt.Errorf("No Rows Affected")
+var ErrorNoRows = fmt.Errorf("No Rows Affected - Not Found")
